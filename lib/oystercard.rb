f7fc6e47dca0eb2f4f27057a6385c4fcddc7ed77
Aaron Diable <aaron.diable@gmail.com>
2019-12-16 15:26:58 +0000

    First test passing

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,11 @@
 class OysterCard
+  attr_reader :balance
 
+  def initialize(balance = 0)
+    @balance = balance
+  end
+
+  def top_up
+    
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,25 @@
 require './lib/oystercard'
 
 describe OysterCard do
+
+  it "creates an instance of OysterCard" do
+    expect(subject).to be_a OysterCard
+  end
+
   it "has a defult balance of 0" do
     expect(subject.balance).to be(0)
+  end
+
+  describe "#top_up" do
+    context 'Allows the user to top_up card' do
+      it "allows the uset to top up money" do
+        expect(subject).to respond_to(:top_up)
+      end
+
+      it "adds a balance to the card" do
+        subject.top_up(10)
+        expect(subject.balance).to eq(10)
+      end
     end
+  end
 end
